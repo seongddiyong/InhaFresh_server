dd382f7a7464f1a944f9985c3e58c020185a99a8
seongddiyong <kodd1102@naver.com>
2022-04-14 16:19:39 +0900

    add selectLocation at serverDao.js

--- Server/serverDao.js
+++ Server/serverDao.js
@@ -1,1 +1,18 @@
 // 디렉팅
+
+// 위치 가져오기
+async function selectLocation(connection, params) {
+    const query =   `
+                    SELECT lng, lat
+                    FROM Location
+                    WHERE arrival = ?
+                    `
+
+    const [row] = await connection.query(query, params);
+
+    return row;
+}
+
+module.exports = {
+    selectLocation,
+}
